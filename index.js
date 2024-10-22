e5cc22cf08ea97a3af6d3465b4ece416049c7291
yashG0 <yashgaurkar0@gmail.com>
2024-10-22 10:01:26 +0530

    update arr loop in index file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log("Hello via Bun!");
 
+const arr = ["hello","i","am","yash"]
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+    
+}
